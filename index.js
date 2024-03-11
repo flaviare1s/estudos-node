38e9440ad9ed1e6fa3247156519e76766f9d56f5
Flávia Reis <flaviarster@gmail.com>
2024-03-11 15:01:35 -0300

    Comecei a montar o formulário com o handlebars

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 const Sequelize = require('sequelize')
 
 
 // Config
     // Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.engine('handlebars', handlebars.engine)
         app.set('view engine', 'handlebars')
     // Conexão com o banco de dados MySQL
     const sequelize = new Sequelize('test', 'root', 'root', {
         host: "localhost",
         dialect: "mysql"
+    })
+
+// Rotas
+    app.get('/cad', function(req, res) {
+        res.render('formulario')
     })
 
 
