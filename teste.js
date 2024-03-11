a4242b9fb3ca27862d993a90d61160fcd97dfcf1
Flávia Reis <flaviarster@gmail.com>
2024-03-11 14:10:16 -0300

    Criando tabelas e inserindo dados com Sequelize

--- teste.js
+++ teste.js
@@ -10,3 +10,46 @@ sequelize.authenticate().then(
     }).catch(function(erro) {
         console.log('Falha ao se conectar! ', erro)
     })
+
+
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.create({
+    titulo: 'Um título qualquer', 
+    conteudo: 'Um conteúdo qualquer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consectetur quo expedita hic? Tempore est in nisi optio illum nemo mollitia iste repudiandae cupiditate corrupti aliquam quibusdam rem, dolorum reiciendis! '
+})
+
+// Postagem.sync({force: true})
+
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: 'João',
+    sobrenome: 'Souza',
+    idade: 33,
+    email: 'teste@email.com'
+})
+
+// Usuario.sync({force: true})
